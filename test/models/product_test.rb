0ef209fcbfc7cad36ed234cf9d6f002b01dc3398
Lalith <lalithr95@gmail.com>
2015-11-23 01:42:15 +0530

    Unit tests for Product

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,46 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "product attributes must not be empty" do
+  	product  = Product.new
+  	assert product.invalid?
+  	assert product.errors[:title].any?
+  	assert product.errors[:description].any?
+  	assert product.errors[:image_url].any?
+  	assert product.errors[:price].any?
+  end
+
+  test "product price must be positive" do
+  	product  = Product.new(title: 'My app',
+  		description: 'My description',
+  		image_url: 'ruby.png')
+  	product.price = -1
+  	assert product.invalid?
+  	assert_equal ["must be greater than or equal to 0.01"], product.errors['price']
+
+  	product.price = 0
+  	assert product.invalid?
+  	assert_equal ["must be greater than or equal to 0.01"], product.errors['price']
+
+  	product.price = 1
+  	assert product.valid?
+  end
+
+  def product_new image_url
+  	product  = Product.new(title: 'My app',
+  		description: 'My description',
+  		image_url: image_url,
+  		price: 12.1)
+  end
+
+  test "image url" do
+  	ok = %w{test.png test.jpg test.gif http://abc.xyz/hello.png}
+  	bad = %w{test.png.more test.png.php}
+  	ok.each do |name|
+  		assert product_new(name).valid?, "#{name} should be valid"
+  	end
+  	bad.each do |name|
+  		assert product_new(name).invalid?, "#{name} is invalid"
+  	end
+  end
 end
