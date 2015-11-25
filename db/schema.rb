d86fa97db5e2a2ec429f82d3f7dfef244e20f4ca
Lalith <lalithr95@gmail.com>
2015-11-25 19:42:28 +0530

    Cart validation and model relationships

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122052457) do
+ActiveRecord::Schema.define(version: 20151124184830) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
