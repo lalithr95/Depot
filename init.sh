68f21f1cd98b1c60f77033ed7572ac334a618532
Lalith <lalithr95@gmail.com>
2015-11-26 16:03:26 +0530

    update

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 export SECRET_KEY_BASE = $(bundle exec rake secret)
 bundle exec rails db:create db:migrate
 bundle exec rails server -b 0.0.0.0
+#docker run --name web -d -p 3000:3000 --link db:pg lalithr95/depot
+#docker run --name db -e POSTGRES_PASSWORD=password -e POSTGRES_USER=rails -d postgres
